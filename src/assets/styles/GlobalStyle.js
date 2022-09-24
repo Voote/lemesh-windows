5d1f448a64c62d0bfa7ad12aa8b5b25faa92a506
Voote <kszygy@gmail.com>
2022-09-24 16:54:54 +0100

    initial commit

---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+// import { createGlobalStyle } from 'styled-components';
+createGlobalStyle
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, *::after, *::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Montserrat', sans-serif;
+        margin: 0;
+        padding: 0;
+        overflow-y: hidden;
+    }
+
+    a, button {        
+        font-family: 'Montserrat', sans-serif;
+    }
+`;
