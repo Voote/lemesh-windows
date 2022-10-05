e1f78204b6bb12f69d9216aa7e815d6289108d35
Voote <kszygy@gmail.com>
2022-10-05 15:53:08 +0100

    colors added to themejs

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,22 +1,25 @@
 export const theme = {
-    colors: {
-      white: '#FFFFFF',
-      lightGrey: '#F7F8FA',
-      grey: '#C0C7D6',
-      darkGrey: '#737C8E',
-      black: '#111111',
-      success: '#8FCB81',
-      error: '#FF8383',
-      warning: '#E1D888',
-      darkPurple: '#C0C7D6',
-      lightPurple: '#ECEFF7',
-    },
-  
-    fontSize: {
-      xl: '24px',
-      l: '17px',
-      m: '12px',
-      s: '11px',
-    },
-  };
-  
+  colors: {
+    white: '#FFFFFF',
+    beige: '#C8AE72',
+    beigeDark: '#B78F68',
+    beigeLight: '#F7F6F1',
+    navyDark: '#151528',
+    lightGrey: '#F7F8FA',
+    grey: '#C0C7D6',
+    darkGrey: '#737C8E',
+    black: '#111111',
+    success: '#8FCB81',
+    error: '#FF8383',
+    warning: '#E1D888',
+    darkPurple: '#C0C7D6',
+    lightPurple: '#ECEFF7',
+  },
+
+  fontSize: {
+    xl: '24px',
+    l: '17px',
+    m: '12px',
+    s: '11px',
+  },
+};
